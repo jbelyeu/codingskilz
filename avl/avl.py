a26d632e82bd9f5eab5341a2e12fd3f6987d2b28
jrbelyeu <jrbelyeu@gmail.com>
2018-11-08 11:34:59 -0700

    added stubs for AVL

---
+++ avl/avl.py
@@ -0,0 +1,28 @@
+class Node():
+    def __init__(self, value):
+        self.height = 0
+        self.left child = None
+        selv.right child = None
+        self.value = value
+    
+    def is_leaf(self):
+
+class AVL():
+    def __init__(self):
+        root = None
+        nodeCount = 0
+
+    #functions
+    def rotate_left(self):
+    
+    def rotate_right(self):
+    
+    def rebalance(self):
+
+    def delete(self, value):
+
+    def insert(self, value):
+
+    def inorder_traverse():
+    
+    def pretty_print():
